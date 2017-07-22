d15927b854fccf4b5fff197b48acb97018890ee6
prabodh44 <prabodhtuladhar44@gmail.com>
2017-07-22 16:38:52 +0545

    Reached Module MiddleWare. Added two middleware components. One that displays the all the environmnet variables and the other showing the Request and Response structure

--- KatanaIntro/Program.cs
+++ KatanaIntro/Program.cs
@@ -31,11 +31,28 @@ namespace KatanaIntro
     {
         public void Configuration(IAppBuilder app)
         {
+
+            app.Use(async (environment, next) =>
+            {
+                foreach (var pair in environment.Environment)
+                {
+                    Console.WriteLine("{0}:{1}", pair.Key, pair.Value);
+                }
+
+                await next();
+            });
+
+            app.Use(async (environment, next) =>
+            {
+                Console.WriteLine("Requesting: " + environment.Request.Path);
+                await next();
+                Console.WriteLine("Response: " + environment.Response.StatusCode);
+
+            });
+
             app.Use<HelloWorldComponent>();
             //this method takes an argument which is the appFunc for next
             //component in the pipeline
-
-
         }
     }
 
